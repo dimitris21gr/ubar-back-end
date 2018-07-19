6ea41c442e29dbf2af2e1d980d5ac4e75dfb8065
dimitris21gr <jimjohnana@yahoo.com>
2018-07-19 19:51:38 +0300

    Initial commit

---
+++ src/main/java/com/ubar/UbarBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.ubar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbarBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbarBackEndApplication.class, args);
+	}
+}
